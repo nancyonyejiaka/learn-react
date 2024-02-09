29a4701ac401d81226094650693448db40e7ce55
Nancy Onyejiaka <nancyonyejiaka@Nancys-MacBook-Pro.local>
2024-02-09 16:59:24 -0500

    Solutions to qcomps 2/9

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,25 @@
 // import Head from 'next/head'
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+import styles from "@/styles/Home.module.css";
+import Profile from "./qcomps/profile_mistake.js";
+import PropProfile from "./components/profile_props.js";
+import FirstComp from "./qcomps/firstcomp.js";
+import Bio from "./qcomps/bios.js";
+import Todo from "./qcomps/todos.js";
+import Gallery from "./qcomps/gallery_props.js";
+import PackingList from "./qcomps/props_item.js";
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+      <PackingList />
+      <Gallery />
+      <PropProfile />
+      <Todo/>
+      <Bio />
+      <Profile />
+      <FirstComp />
     </div>
-  )
+  );
 }
--- src/pages/qcomps/gallery_props.js
+++ src/pages/qcomps/gallery_props.js
@@ -1,55 +1,59 @@
+function Profile({ person }) {
+  return (
+    <div>
+      <section className="profile">
+        <h2>person.name</h2>
+        <img
+          className="avatar"
+          src={`https://i.imgur.com/${person.imageId}.jpg`}
+          alt={person.name}
+          width={70}
+          height={70}
+        />
+        <ul>
+          <li>
+            <b>Profession: </b>
+            {person.profession}
+          </li>
+          <li>
+            <b>Awards: {person.totalAwards} </b>({person.awards})
+          </li>
+          <li>
+            <b>Discovered: </b>
+            {person.discovered}
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+}
+
 export default function Gallery() {
   return (
     <div>
-      <h1>Notable Scientists</h1>
-      <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/szV5sdGs.jpg'
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            physicist and chemist
-          </li>
-          <li>
-            <b>Awards: 4 </b>
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (element)
-          </li>
-        </ul>
-      </section>
-      <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/YfeOqp2s.jpg'
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            geochemist
-          </li>
-          <li>
-            <b>Awards: 2 </b>
-            (Miyake Prize for geochemistry, Tanaka Prize)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            a method for measuring carbon dioxide in seawater
-          </li>
-        </ul>
-      </section>
+      <h1>Notable</h1>
+      <Profile
+        person={{
+          name: "Maria Skłodowska-Curie",
+          imageId: "szV5sdGs",
+          profession: "physicist and chemist",
+          totalAwards: "4",
+          awards:
+            "Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal",
+          discovered: "polonium (element)",
+        }}
+      />
+
+      <Profile
+        person={{
+          name: "Katsuko Saruhashi",
+          imageId: "YfeOqp2s",
+          profession: "geochemist",
+          totalAwards: "2",
+          awards: "Miyake Prize for geochemistry, Tanaka Prize",
+          discovered: "a method for measuring carbon dioxide in seawater",
+        }}
+      />
     </div>
   );
 }
